cb827b8c9266f2c5da2e390a717a5709f4bd0c6c
polukarp <perzhul.works@gmail.com>
2024-09-29 13:12:45 +0300

    test(parser): add test case for integer parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -146,3 +146,39 @@ func TestIdentifierExpression(t *testing.T) {
 		t.Errorf("ident value not %s. got=%s", "foobar", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain enough statements. got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T",
+			program.Statements)
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Fatalf("literal.Value not 5. got=%d", literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Fatalf("literal.TokenLiteral() not '5'. got=%s",
+			literal.TokenLiteral())
+	}
+
+}
