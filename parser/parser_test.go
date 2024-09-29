94f6b2e0f352caad4c379cc25b6619315804466f
polukarp <perzhul.works@gmail.com>
2024-09-29 11:22:24 +0300

    test(parser): add test for identifier expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -115,3 +115,34 @@ func TestReturnStatements(t *testing.T) {
 		}
 	}
 }
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain enough statements. got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident value not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
